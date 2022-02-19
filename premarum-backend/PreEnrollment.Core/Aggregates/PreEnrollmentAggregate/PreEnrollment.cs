0ec6fe01751832cb177cb97e4ba61c04942c5ec7
Kenenth Rosario <kenneth.rosario7@upr.edu>
2022-02-19 15:53:41 -0400

    Updated domain model

--- premarum-backend/PreEnrollment.Core/Aggregates/PreEnrollmentAggregate/PreEnrollment.cs
+++ premarum-backend/PreEnrollment.Core/Aggregates/PreEnrollmentAggregate/PreEnrollment.cs
@@ -10,7 +10,6 @@ public class PreEnrollment: Aggregate
         Name = name;
         StudentId = studentId;
         SemesterId = semesterId;
-        Selections = new List<SemesterOffer>();
         SelectionIds = new HashSet<PreEnrollmentSelection>();
     }
     
@@ -20,16 +19,9 @@ public class PreEnrollment: Aggregate
         SelectionIds = new HashSet<PreEnrollmentSelection>(selections);
     }
 
-    public PreEnrollment(long? id, string name, long studentId, long semesterId, IEnumerable<SemesterOffer> semesterOffers)
-    {
-        Selections = new List<SemesterOffer>(semesterOffers);
-    }
-    
     public long? Id { get; private set; }
     public string Name { get; private set; }
     public long StudentId { get; private set; }
     public long SemesterId { get; private set; }
-    
     public IEnumerable<PreEnrollmentSelection> SelectionIds { get; private set; }
-    public IEnumerable<SemesterOffer> Selections { get; private set; }
 }
