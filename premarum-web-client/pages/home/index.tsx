ec28edd129c34f778081c51f75b62cff5a5825b5
Yavier Mari <yavier.mari@upr.edu>
2022-02-23 15:30:08 -0400

    initial commit, navbar done, ready for pages

---
+++ premarum-web-client/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import { Grid, Typography } from '@mui/material'
+
+export default function Home() {
+  return (
+        <Grid container direction="row">
+
+            <Grid item>
+                <Typography>Home Page</Typography>
+            </Grid>
+          
+        </Grid>
+  )
+}
