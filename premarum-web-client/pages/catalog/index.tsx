7e1356c44a582ccfdf3e4fb761590fe5d1255408
Yavier Mari <yavier.mari@upr.edu>
2022-02-25 12:23:40 -0400

    Navigation pull (#14)
    * added topbar/sidebar
    
    * navbars working
    
    * initial commit, navbar done, ready for pages

---
+++ premarum-web-client/pages/catalog/index.tsx
@@ -0,0 +1,13 @@
+import { Grid, Typography } from '@mui/material'
+
+export default function Catalog() {
+    return (
+        <Grid container direction="row">
+
+            <Grid item>
+                <Typography>Course Catalog Page</Typography>
+            </Grid>
+          
+        </Grid>
+    )
+}
---
+++ premarum-web-client/pages/profile/index.tsx
@@ -0,0 +1,13 @@
+import { Grid, Typography } from '@mui/material'
+
+export default function Profile() {
+    return (
+        <Grid container direction="row">
+
+            <Grid item>
+                <Typography>Profile Page</Typography>
+            </Grid>
+          
+        </Grid>
+    )
+}
