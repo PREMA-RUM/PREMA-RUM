1aaaf7c77f74e903ccc87284e6006daab6cb03fd
Yavier Mari <yavier.mari@upr.edu>
2022-02-28 14:00:50 -0400

    finished profile add courses modal

--- premarum-web-client/pages/profile/index.tsx
+++ premarum-web-client/pages/profile/index.tsx
@@ -1,20 +1,45 @@
-import { Button, Card, Divider, Grid, TextField, Typography } from '@mui/material'
+import { Autocomplete, Button, Card, CardActions, CardContent, CardHeader, Divider, Fade, Grid, Modal, TextField, Typography } from '@mui/material'
 import { AddRounded } from '@mui/icons-material'
+import React from 'react';
+import { grey } from '@mui/material/colors';
 
-function AddButton() {
-    return(
-        <Button
-            startIcon={<AddRounded/>}
-            variant="contained"
-            sx={classes.addCoursesButton}
-        >
-            Add Courses Taken
-        </Button>
-    )
-}
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
+const top100Films = [
+    { title: 'The Shawshank Redemption', year: 1994 },
+    { title: 'The Godfather', year: 1972 },
+    { title: 'The Godfather: Part II', year: 1974 },
+    { title: 'The Dark Knight', year: 2008 },
+    { title: '12 Angry Men', year: 1957 },
+    { title: "Schindler's List", year: 1993 },
+    { title: 'Pulp Fiction', year: 1994 },
+]
 
 export default function Profile() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleModalOpen = () => {
+        setOpen(true);
+    };
+    
+    const handleModalClose = () => {
+        setOpen(false);
+    };
+
+    function AddButton() {
+        return(
+            <Button
+                startIcon={<AddRounded/>}
+                variant="contained"
+                sx={classes.addCoursesButton}
+                onClick={handleModalOpen}
+            >
+                Add Courses Taken
+            </Button>
+        )
+    }
+
     return (
+        <>
         <Grid container direction="column">
 
             <Grid item>
@@ -50,7 +75,7 @@ export default function Profile() {
                         <Divider/>
                         
                         <Grid container direction="column" alignContent='center' justifyContent="center" sx={classes.coursesContainer}>
-                            <Typography>Nothing here yet...</Typography>
+                            <Typography>Nothing here yet... :(</Typography>
                             <AddButton/>
                         </Grid>
                         
@@ -59,6 +84,49 @@ export default function Profile() {
             </Grid>
           
         </Grid>
+
+        <Modal
+            open={open}
+            onClose={handleModalClose}
+            closeAfterTransition
+        >
+            <Fade in={open}>
+                <Grid container direction="row" justifyContent="center" alignItems="center" sx={classes.modalGridMain}>
+                    <Card sx={classes.modalCard}>
+                        <CardHeader
+                            title="Add Courses Taken"
+                        />
+
+                        <Divider/>
+
+                        <CardContent sx={classes.cardContent}>
+                            <Autocomplete
+                                multiple
+                                id="tags-outlined"
+                                options={top100Films}
+                                getOptionLabel={(option) => option.title}
+                                filterSelectedOptions
+                                renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    label="Course Selection"
+                                    placeholder="Add Courses..."
+                                />
+                                )}
+                            />
+                        </CardContent>
+
+                        <Divider/>
+
+                        <CardActions sx={classes.cardActions}>
+                            <Button onClick={handleModalClose}>Cancel</Button>
+                            <Button onClick={handleModalClose}>Submit</Button>
+                        </CardActions>
+                    </Card>
+                </Grid>
+            </Fade>
+        </Modal>
+        </>
     )
 }
 
@@ -84,15 +152,32 @@ const useStyles = {
     contentCard: {
         backgroundColor: 'secondary.light',
         padding: '15px',
-        height: '100%',
-        minHeight: 450
+        minHeight: '80vh',
     },
     contentText: {
         padding: '2px 10px',
     },
     coursesContainer: {
         marginTop: 10
-    }
+    },
+    modalGridMain: {
+        width: '100%',
+        height: '90%',
+    },
+    modalCard: {
+        width: '60%',
+        minHeight: '30%',
+        backgroundColor: grey[50],
+    },
+    cardContent: {
+        width: '100%',
+        height: '100%',
+        padding: '50px 20px',
+    },
+    cardActions: {
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+    },
   };
 
 const classes = useStyles;
