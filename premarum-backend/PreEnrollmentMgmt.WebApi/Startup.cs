b2d19e36bf37374ac92f88633adc36e69af26208
Kenneth Rosario <kenneth.rosario7@upr.edu>
2022-02-28 12:29:48 -0400

    Automated web api and database population and init for development. (#15)
    * Automated database population and init for development. Automated asp.net core connection to db
    
    * Removed comment I had forgotten

---
+++ premarum-backend/PreEnrollmentMgmt.WebApi/Startup.cs
@@ -0,0 +1,18 @@
+using PreEnrollmentMgmt.Application.Repositories;
+using PreEnrollmentMgmt.Core.Repositories;
+using PreEnrollmentMgmt.Core.Services;
+
+namespace PreEnrollmentMgmt.WebApi;
+
+public static class Startup
+{
+    public static void ConfigureProgramServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IPreEnrollmentRepository, PreEnrollmentRepository>();
+        builder.Services.AddScoped<IStudentRepository, StudentRepository>();
+        builder.Services.AddScoped<ISemesterOfferRepository, SemesterOfferRepository>();
+        builder.Services.AddScoped<ITransactionManager, TransactionManager>();
+        builder.Services.AddScoped<PreEnrollmentService, PreEnrollmentService>();
+        builder.Services.AddScoped<StudentValidationService, StudentValidationService>();
+    }
+}
