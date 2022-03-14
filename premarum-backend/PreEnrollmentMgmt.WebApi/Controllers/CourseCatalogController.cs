2803dd09c66c454d02f86ffe62e1fc25cd3a5c65
Kenneth Rosario <kenneth.rosario7@upr.edu>
2022-03-14 13:58:02 -0400

    Added simple course catalog read routes (#26)

---
+++ premarum-backend/PreEnrollmentMgmt.WebApi/Controllers/CourseCatalogController.cs
@@ -0,0 +1,43 @@
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PreEnrollmentMgmt.Core.Repositories;
+using PreEnrollmentMgmt.WebApi.Controllers.DTOS;
+
+namespace PreEnrollmentMgmt.WebApi.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/[controller]")]
+public class CourseCatalogController : ControllerBase
+{
+    private readonly ISemesterOfferRepository _semesterOfferRepository;
+    private readonly IMapper _mapper;
+
+    public CourseCatalogController(ISemesterOfferRepository semesterOfferRepository, IMapper mapper)
+    {
+        _semesterOfferRepository = semesterOfferRepository;
+        _mapper = mapper;
+    }
+
+    [HttpGet("Semester")]
+    public async Task<IEnumerable<SemesterDTO>> GetAvailableSemesters()
+    {
+        var result = await _semesterOfferRepository.GetAvailableSemesters();
+        return _mapper.Map<IEnumerable<SemesterDTO>>(result);
+    }
+    
+    [HttpGet("Department")]
+    public async Task<IEnumerable<DepartmentDTO>> GetAvailableDepartments()
+    {
+        var result = await _semesterOfferRepository.GetAvailableDepartments();
+        return _mapper.Map<IEnumerable<DepartmentDTO>>(result);
+    }
+    
+    [HttpGet("Course")]
+    public async Task<IEnumerable<CourseDTO>> GetAvailableCourses()
+    {
+        var result = await _semesterOfferRepository.GetAvailableCourses();
+        return _mapper.Map<IEnumerable<CourseDTO>>(result);
+    }
+}
---
+++ premarum-backend/PreEnrollmentMgmt.WebApi/Controllers/DTOS/DepartmentDTO.cs
@@ -0,0 +1,7 @@
+namespace PreEnrollmentMgmt.WebApi.Controllers.DTOS;
+
+public class DepartmentDTO
+{
+    public int DepartmentId { get; set; }
+    public string DepartmentName { get; set; }
+}
