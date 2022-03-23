d793f814661a66eb24a597f838cf86a2879461dc
Kenneth Rosario <kenneth.rosario7@upr.edu>
2022-03-23 18:17:59 -0400

    Redirecting to empty html page

--- premarum-web-client/utility/constants.ts
+++ premarum-web-client/utility/constants.ts
@@ -4,4 +4,5 @@ export const HOST = process.env.NEXT_PUBLIC_HOST;
 
 export const TOKEN_REQUEST:PopupRequest = {
     scopes:["api://83c3cbe7-f00e-4ea8-87d8-bf4d75690f17/access_as_user"],
+    redirectUri:"/blank.html"
 }
