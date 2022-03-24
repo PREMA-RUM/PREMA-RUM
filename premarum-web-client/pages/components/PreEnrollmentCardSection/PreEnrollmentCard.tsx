e86664c791e1484abd6db0b8b5856657665b7bbf
Kenenth Rosario <kenneth.rosario7@upr.edu>
2022-03-23 22:45:11 -0400

    First version of PreEnrollment Grouping by Semester

---
+++ premarum-web-client/pages/components/PreEnrollmentCardSection/PreEnrollmentCard.tsx
@@ -0,0 +1,116 @@
+import {DeleteRounded} from "@mui/icons-material";
+import {Card, CardHeader, Divider, Avatar, Button, ButtonGroup} from "@mui/material";
+import {
+    IPreEnrollmentResponse,
+    IPreEnrollmentSelectionResponse,
+    ISemesterResponse
+} from "../../../utility/requests/responseTypes";
+
+
+function OptionsButton() {
+    return (
+        <ButtonGroup variant="contained" disableElevation sx={classes.buttonGroup}>
+            <Button>Edit</Button>
+            <Button size="small">
+                <DeleteRounded/>
+            </Button>
+        </ButtonGroup>
+    )
+}
+
+export type PreEnrollmentCardItemProps = {
+    preEnrollment: IPreEnrollmentResponse
+}
+
+function PreEnrollmentCardItem({preEnrollment}: PreEnrollmentCardItemProps) {
+
+    function getTotalCredits(): number {
+        return preEnrollment
+            .selections
+            .reduce((currCredits: number, currentVal: IPreEnrollmentSelectionResponse) => {
+                return currCredits + currentVal.course.courseCredit
+            }, 0);
+    }
+
+    function getCourseStringList(): string {
+        let finalString = ""
+        if (preEnrollment.selections.length === 0) {
+            return finalString
+        }
+        for (let i = 0; i < 3; i++) {
+            if (i === preEnrollment.selections.length - 1) {
+                finalString += `${preEnrollment.selections[i].course.courseCode}`
+            } else if (i < preEnrollment.selections.length) {
+                finalString += `${preEnrollment.selections[i].course.courseCode}, `
+            }
+        }
+        if (preEnrollment.selections.length > 3) {
+            finalString += "..."
+        }
+        return finalString;
+    }
+
+    return (
+        <>
+            <Card elevation={0} square sx={classes.itemCard}>
+                <CardHeader
+                    avatar={
+                        <Avatar sx={{bgcolor: 'secondary.dark'}} aria-label={`${preEnrollment.name}`}>
+                            {preEnrollment.name[0]}
+                        </Avatar>
+                    }
+                    action={
+                        <OptionsButton/>
+                    }
+                    title={`${preEnrollment.name} - ${getTotalCredits()} Credits`}
+                    subheader={getCourseStringList()}
+                />
+            </Card>
+            <Divider/>
+        </>
+    )
+}
+
+export type PreEnrollmentCardProps = {
+    group: IPreEnrollmentResponse[],
+    semester: ISemesterResponse
+}
+
+export default function PreEnrollmentCard({group, semester}: PreEnrollmentCardProps) {
+    console.log(group, semester)
+    return (
+        <Card sx={classes.containerCard}>
+            <CardHeader
+                sx={classes.mainHeader}
+                title={`Semester - ${semester.term} - ${semester.year}`}
+            />
+            <Divider/>
+            {group.map((currVal: IPreEnrollmentResponse, index: number) => {
+                return <PreEnrollmentCardItem preEnrollment={currVal} key={index + 8888}/>
+            })}
+        </Card>
+    )
+}
+
+const useStyles = {
+    containerCard: {
+        marginBottom: 1.5
+    },
+    mainHeader: {},
+    mainContent: {
+        width: '100%',
+    },
+    buttonGroup: {
+        marginLeft: 5,
+        marginTop: 0.8,
+        paddingRight: '10px'
+    },
+    itemGrid: {
+        width: '100%',
+    },
+    itemCard: {
+        width: '100%'
+    }
+};
+
+const classes = useStyles;
