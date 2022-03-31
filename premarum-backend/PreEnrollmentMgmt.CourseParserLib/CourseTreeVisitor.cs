4529d44e53e6363c8f35c28afbb05121b848f09f
Kenenth Rosario <kenneth.rosario7@upr.edu>
2022-03-31 00:19:43 -0400

    Added Inital Grammar and Skeleton Classes

---
+++ premarum-backend/PreEnrollmentMgmt.CourseParserLib/CourseTreeVisitor.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+using Antlr4.Runtime.Tree;
+
+namespace PreEnrollmentMgmt.CourseParserLib;
+
+
+
+public class CourseTreeVisitor: CourseGrammarBaseVisitor<CourseVisitorOutput> {
+    public CourseTreeVisitor(ImmutableHashSet<string> coursesTaken)
+    {
+        CoursesTaken = coursesTaken;
+    }
+
+    public ImmutableHashSet<string> CoursesTaken { get; set; }
+}
---
+++ premarum-backend/PreEnrollmentMgmt.CourseParserLib/Valuetype.cs
@@ -0,0 +1,8 @@
+namespace PreEnrollmentMgmt.CourseParserLib;
+
+public enum Valuetype
+{
+    COURSE,
+    OUTPUT,
+    UNKNOWN
+}
