55cb8a5dd30131acacb11449b0253c6d9a7389e9
joseriveramorales <joserivervmorales@gmail.com>
2022-04-13 17:15:05 -0400

    Fixing how PM times are handled.

--- AdHoc DB inserts/generating_timeSlots.py
+++ AdHoc DB inserts/generating_timeSlots.py
@@ -1,9 +1,7 @@
-
-dayLetterToDayIds = {'L': 1,'M': 2,'W': 3,'J': 4,'V': 5,'S': 6}
+dayLetterToDayIds = {'L': 1, 'M': 2, 'W': 3, 'J': 4, 'V': 5, 'S': 6}
 
 
 class TimeSlotGenerator:
-
 
     def trimAndSelectDays(self, daysStr):
         daysStrTrimmed = daysStr.strip()
@@ -15,17 +13,17 @@ class TimeSlotGenerator:
 
     def getStartTime(self):
         startTime = self.startTime[0:5]
-        startTime = startTime.strip()
+        PMIndicator = self.endTime[11:12]
+        if PMIndicator == 'p' and int(startTime[0:2]) != 12:
+            startTime = str(int(startTime[0:2]) + 12) + (startTime[2:len(startTime)])
         return startTime
 
     def getEndTime(self):
         endTime = self.endTime[6:11]
         PMIndicator = self.endTime[11:12]
-        if(PMIndicator == 'p'):
+        if PMIndicator == 'p' and int(endTime[0:2]) != 12:
             endTime = str(int(endTime[0:2]) + 12) + (endTime[2:len(endTime)])
         return endTime
-
-
 
     def __init__(self, timeSlotTuple):
         self.so_id = timeSlotTuple[0]
@@ -36,14 +34,14 @@ class TimeSlotGenerator:
 
     def timeSlotInsert(self):
         for i in self.trimAndSelectDays(self.days):
-            print('insert into "TimeSlot" (ts_start_time, ts_end_time, so_id, d_id) VALUES (%s,%s,%d,%d);'  % (self.getStartTime(), self.getEndTime(), self.so_id, i))
+            print(
+                "insert into \"TimeSlot\" ( 'ts_start_time', 'ts_end_time' , so_id, d_id) VALUES ('%s','%s',%d,%d);" % (
+                self.getStartTime(), self.getEndTime(), self.so_id, i))
 
-inserts = [ (1846, '12:30- 1:20p', 'W      ', '60'),
-            (1847, '12:30- 1:20p', 'W      ', '61'),
-            (1848, ' 3:30- 5:20p', 'LW     ', '90')]
+
+inserts = [(1, '12:00- 1:15p', 'MJ     ', '66'),
+           (1848, ' 3:30- 5:20p', 'LW     ', '90')]
 
 for insert in inserts:
     TSG = TimeSlotGenerator(insert)
     TSG.timeSlotInsert()
-        
-        
