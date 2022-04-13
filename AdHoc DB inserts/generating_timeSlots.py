ce080c6f0abb9480e75e216061e2688692f3aae3
joseriveramorales <joserivervmorales@gmail.com>
2022-04-13 16:09:35 -0400

    Python Script to use UPRM data

---
+++ AdHoc DB inserts/generating_timeSlots.py
@@ -0,0 +1,49 @@
+
+dayLetterToDayIds = {'L': 1,'M': 2,'W': 3,'J': 4,'V': 5,'S': 6}
+
+
+class TimeSlotGenerator:
+
+
+    def trimAndSelectDays(self, daysStr):
+        daysStrTrimmed = daysStr.strip()
+        daysList = list(daysStrTrimmed)
+        daysIdList = list()
+        for i in daysList:
+            daysIdList.append(dayLetterToDayIds[i])
+        return daysIdList
+
+    def getStartTime(self):
+        startTime = self.startTime[0:5]
+        startTime = startTime.strip()
+        return startTime
+
+    def getEndTime(self):
+        endTime = self.endTime[6:11]
+        PMIndicator = self.endTime[11:12]
+        if(PMIndicator == 'p'):
+            endTime = str(int(endTime[0:2]) + 12) + (endTime[2:len(endTime)])
+        return endTime
+
+
+
+    def __init__(self, timeSlotTuple):
+        self.so_id = timeSlotTuple[0]
+        self.startTime = timeSlotTuple[1]
+        self.endTime = timeSlotTuple[1]
+        self.days = timeSlotTuple[2]
+        self.seccion = timeSlotTuple[3]
+
+    def timeSlotInsert(self):
+        for i in self.trimAndSelectDays(self.days):
+            print('insert into "TimeSlot" (ts_start_time, ts_end_time, so_id, d_id) VALUES (%s,%s,%d,%d);'  % (self.getStartTime(), self.getEndTime(), self.so_id, i))
+
+inserts = [ (1846, '12:30- 1:20p', 'W      ', '60'),
+            (1847, '12:30- 1:20p', 'W      ', '61'),
+            (1848, ' 3:30- 5:20p', 'LW     ', '90')]
+
+for insert in inserts:
+    TSG = TimeSlotGenerator(insert)
+    TSG.timeSlotInsert()
+        
+        
