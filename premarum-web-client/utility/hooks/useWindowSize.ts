07a8a821f20fd14ca64225fa5aaee4e40ac8482b
Kenneth Rosario <kenneth.rosario7@upr.edu>
2022-04-15 01:00:50 -0400

    Removing listener just using inital size

--- premarum-web-client/utility/hooks/useWindowSize.ts
+++ premarum-web-client/utility/hooks/useWindowSize.ts
@@ -25,13 +25,13 @@ export function useWindowSize() {
         // only execute all the code below in client side
         if (typeof window !== 'undefined') {
             // Add event listener
-            window.addEventListener("resize", handleResize);
+            // window.addEventListener("resize", handleResize);
 
             // Call handler right away so state gets updated with initial window size
             handleResize();
 
             // Remove event listener on cleanup
-            return () => window.removeEventListener("resize", handleResize);
+            // return () => window.removeEventListener("resize", handleResize);
         }
     }, []); // Empty array ensures that effect is only run on mount
     return windowSize;
