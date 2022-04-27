46c8a5307b9c754cee60f182eb868aa740afea56
Kenneth Rosario <kenneth.rosario7@upr.edu>
2022-04-27 12:27:20 -0400

    Used CultureInfo to specify ToString Format (#93)

--- premarum-backend/PreEnrollmentMgmt.WebApi/Controllers/DTOS/DTOMapping.cs
+++ premarum-backend/PreEnrollmentMgmt.WebApi/Controllers/DTOS/DTOMapping.cs
@@ -1,3 +1,4 @@
+using System.Globalization;
 using AutoMapper;
 using PreEnrollmentMgmt.Core.Entities;
 using PreEnrollmentMgmt.Core.Entities.ComputedEntities;
@@ -13,20 +14,50 @@ public class DTOMapping : Profile
         CreateMap<Student, StudentDTO>();
         CreateMap<Professor, ProfessorDTO>();
         CreateMap<TimeSlot, TimeSlotDTO>()
+            .ForMember(dest => dest.StartTime, opt => 
+            {
+                opt.MapFrom(src => src.StartTime.ToString(
+                new CultureInfo("en-US")
+                ));
+            })
+            .ForMember(dest => dest.EndTime, opt =>
+            {
+                opt.MapFrom(src => src.EndTime.ToString(
+                    new CultureInfo("en-US")
+                ));
+            })
             .ForMember(dest => dest.Day,
                 opt => { opt.MapFrom(src => src.WeekDay.Name); });
         CreateMap<SemesterOffer, PreEnrollmentSemesterOfferDTO>();
         CreateMap<Semester, SemesterDTO>()
             .ForMember(dest => dest.Term, opt => { opt.MapFrom(src => src.Term.TermName); });
         CreateMap<OverlappingPreEnrollmentSelections, ConflictingSelectionDTO>()
-            .ForMember(dest => dest.StartTimeA,
-                opt => { opt.MapFrom(src => src.StartTimeA.ToString()); })
+            .ForMember(dest => dest.StartTimeA, opt =>
+                {
+                    opt.MapFrom(src => src.StartTimeA.ToString(
+                        new CultureInfo("en-US")
+                    ));
+                })
             .ForMember(dest => dest.StartTimeB,
-                opt => { opt.MapFrom(src => src.StartTimeB.ToString()); })
-            .ForMember(dest => dest.EndTimeA,
-                opt => { opt.MapFrom(src => src.EndTimeA.ToString()); })
+                opt =>
+                {
+                    opt.MapFrom(src => src.StartTimeB.ToString(
+                        new CultureInfo("en-US")
+                    ));
+                })
+            .ForMember(dest => dest.EndTimeA, opt =>
+                {
+                    opt.MapFrom(src => src.EndTimeA.ToString(
+                        new CultureInfo("en-US")    
+                    ));
+                })
             .ForMember(dest => dest.EndTimeB,
-                opt => { opt.MapFrom(src => src.EndTimeB.ToString()); });
+                opt =>
+                {
+                    opt.MapFrom(src => src.EndTimeB.ToString(
+                        new CultureInfo("en-US")    
+                    ));
+                });
         CreateMap<Department, DepartmentDTO>();
         CreateMap<CoursesTaken, CoursesTakenDTO>();
     }
