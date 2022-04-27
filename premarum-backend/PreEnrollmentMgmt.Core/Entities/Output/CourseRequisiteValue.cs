0e8cd3bff07d663c15028b84b8a997872348fbe2
joseriveramorales <joserivervmorales@gmail.com>
2022-04-27 12:36:00 -0400

    Had to specify the Equality member for the CourseRequisiteValue.cs.

--- premarum-backend/PreEnrollmentMgmt.Core/Entities/Output/CourseRequisiteValue.cs
+++ premarum-backend/PreEnrollmentMgmt.Core/Entities/Output/CourseRequisiteValue.cs
@@ -8,6 +8,24 @@ public class CourseRequisiteValue
         StudentHasRequisites = studentHasRequisites;
     }
 
+    protected bool Equals(CourseRequisiteValue other)
+    {
+        return CourseId == other.CourseId && StudentHasRequisites == other.StudentHasRequisites;
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((CourseRequisiteValue) obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(CourseId, StudentHasRequisites);
+    }
+
     public int CourseId { get; set; }
     public bool StudentHasRequisites { get; set; }
 }
