d898bb7cad98639c98f45efdb4bfe42c92b21314
joseriveramorales <joserivervmorales@gmail.com>
2022-05-02 01:33:23 -0400

    not an array of PreEnrollment Response

--- premarum-web-client/utility/requests/getStudentMissingCourses.ts
+++ premarum-web-client/utility/requests/getStudentMissingCourses.ts
@@ -3,7 +3,7 @@ import {getAuthToken} from "../helpers";
 import {HOST, pca} from "../constants";
 import axios from "axios";
 
-export default async function getStudentMissingCourses(preEnrollmentId: Number): Promise<IMissingPreEnrollmentsResponse[]> {
+export default async function getStudentMissingCourses(preEnrollmentId: Number): Promise<IMissingPreEnrollmentsResponse> {
     const token = await getAuthToken(pca)
     return (await axios.post(`${HOST}/PreEnrollment/${preEnrollmentId}/Requisites`, {}, {
         headers: {
